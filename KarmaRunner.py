64738316a5e6662027f986339506e9644ea7e7a7
knee-cola <nikola.derezic@gmail.com>
2017-02-02 15:47:41 +0100

    BugFix: Hotkeys were messing up how the "Escape" buttone interacts with other IDE elements (search box, multiple cursors etc). Now instead of using hotkeys, the output pannel is shown by an [OnPostSave] event handler is used, which detects if the panel should be displayed or not.

--- KarmaRunner.py
+++ KarmaRunner.py
@@ -5,31 +5,60 @@ import sublime_plugin
 # http://stackoverflow.com/questions/41768673/let-sublime-choose-among-two-similar-build-systems/41815033
 # http://www.programcreek.com/python/example/52601/sublime.load_settings
 
+_KarmaIsRunning = False
+_showKarmaForFileType = ""
+
 class run_karmaCommand(sublime_plugin.WindowCommand):
 	def run(self):
-		# http://www.programcreek.com/python/example/52601/sublime.load_settings
+		global _KarmaIsRunning
+		global _showKarmaForFileType
 
-		# this flag will ENABLE the keyboard shotcut which prevents Esc button
-		# from hiding the "Build Result Panel"
-		sublime.load_settings("Preferences.sublime-settings").set("karma_running", True)
+		# run karma only iof it's not currently running
+		if _KarmaIsRunning==False:
 
-		# getting the name of the build system - the one stated in the sublime-project file
-		build_system_name = sublime.load_settings("KarmaRunner.sublime-settings").get("build_system_name")
+			settings = sublime.load_settings("KarmaRunner.sublime-settings")
+			
+			_showKarmaForFileType = "." + settings.get("show_karma_for_file_type")
+			# listen for settings change
+			settings.add_on_change("show_karma_for_file_type", lambda: self.reload_settings(settings))
 
-		# run the build task defined in the sublime-project file
-		self.window.run_command("build", args={"name": build_system_name})
-		self.window.run_command("build", args={"name": build_system_name})
+			# getting the name of the build system - the one stated in the sublime-project file
+			build_system_name = settings.get("build_system_name")
 
+			# run the build task defined in the sublime-project file
+			self.window.run_command("build", args={"name": build_system_name})
+			_KarmaIsRunning = True
+
+	# event hander for settings change
+	def reload_settings(self, settings):
+		global _showKarmaForFileType
+		_showKarmaForFileType = "." + settings.get("show_karma_for_file_type")
 
 class kill_karmaCommand(sublime_plugin.WindowCommand):
 	def run(self):
-		# http://www.programcreek.com/python/example/52601/sublime.load_settings
-		settings = sublime.load_settings("Preferences.sublime-settings")
-		# this flag will DISABLE the keyboard shotcut which prevents Esc button
-		# from hiding the "Build Result Panel"
-		settings.set("karma_running", False)
-		
-		# ask SublimeANSI to kill build process
-		self.window.run_command("ansi_color_build", args={"kill": True})
-		# hide the output panel
-		self.window.run_command("hide_panel", args={"cancel": True})
+		global _KarmaIsRunning
+
+		# do nothing of Karma is not running
+		if _KarmaIsRunning:
+			# ask SublimeANSI to kill build process
+			self.window.run_command("ansi_color_build", args={"kill": True})
+			# hide the output panel
+			self.window.run_command("hide_panel", args={"cancel": True})
+			_KarmaIsRunning = False
+
+			# stop listening on settings change
+			sublime.load_settings("KarmaRunner.sublime-settings").clear_on_change("show_karma_for_file_type")
+
+# Every time a file is saved show the output window,
+# so that the test results can be seen
+class karmaRunner_OnPostSave(sublime_plugin.EventListener):
+	def on_post_save(self, view):
+		global _KarmaIsRunning
+		global _showKarmaForFileType
+
+		# show panel only if Karma is currently running
+		if _KarmaIsRunning:
+			print(_showKarmaForFileType)
+			# show panel only if the file type is JavaScript
+			if view.file_name()[-3:]==_showKarmaForFileType:
+				view.window().run_command("show_panel", args={"panel": "output.exec"})
